703776718d255f65c4f6322515a6f5a255874a38
m2nna <robertm2nna@gmail.com>
2018-04-24 15:04:09 +0200

    MSER feature detection from HSV image

--- ImageAquisition.m
+++ ImageAquisition.m
@@ -8,7 +8,7 @@ addpath('Mex')
 
 
 KinectHandles=mxNiCreateContext();
-DlgH = figure;
+%DlgH = figure;
 H = uicontrol('Style', 'PushButton', ...
                     'String', 'Break', ...
                     'Callback', 'delete(gcbf)');
@@ -16,23 +16,35 @@ H = uicontrol('Style', 'PushButton', ...
 cam_image=mxNiPhoto(KinectHandles); 
 cam_image=permute(cam_image,[3 2 1]);
 
-subplot(1,2,1), h1=imshow(cam_image);
+%subplot(1,2,1), h1=imshow(cam_image);
 
 
 
-while (ishandle(H));
+while (ishandle(H))
      cam_image=mxNiPhoto(KinectHandles);cam_image=permute(cam_image,[3 2 1]);
-    B=rgb2gray(cam_image); 
- corners=detectBRISKFeatures(B);
- strongest=selectStrongest(corners,500);
- [vector, vis]=extractFeatures(B,strongest,'Method','SURF');
+     cam_image=imadjust(cam_image,stretchlim(cam_image));
+     
+     B=rgb2hsv(cam_image);
+     
+     C=B(:,:,1);
+     D=B(:,:,2);
+     
+     %C=im2bw(C,graythresh(C));
+     regions_sat=detectMSERFeatures(D);
+     regions_hue=detectMSERFeatures(C);
+ %corners=detectSURFFeatures(B);
+ %strongest=selectStrongest(corners,100);
+ %[vector, vis]=extractFeatures(B,strongest,'Method','SURF');
  
 
  
- 
- subplot(1,2,2), h2=imshow(B);
+ subplot(1,2,1), h1=imshow(C);title('MSER feature detection on Hue');
  hold on;
- plot(vis);
+ plot(regions_hue,'showPixelList',true,'showEllipses',false);
+ hold off;
+ subplot(1,2,2), h2=imshow(D);title('on Saturation');
+ hold on;
+ plot(regions_sat,'showPixelList',true,'showEllipses',false);
  
  hold off;
     
