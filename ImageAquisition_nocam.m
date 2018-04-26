2d4fbaee633b5e0d18bd0c00d7a582f1ba2fca48
StefanNah <snahst16@student.aau.dk>
2018-04-26 14:16:29 +0200

    NTSC FTW

--- ImageAquisition_nocam.m
+++ ImageAquisition_nocam.m
@@ -26,18 +26,33 @@ clear all;
 
 
 %% Import
-cam_image =im2double(imread('lego_own.jpg'));
-%cam_image =imread('black_separated_light.png');
+%cam_image =im2double(imread('lego_own.jpg'));
+cam_image =imread('black_separated_light.png');
 size(cam_image)
 figure; imshow(cam_image);
-cam_image=imresize(cam_image,0.2);
+%cam_image=imresize(cam_image,0.2);
 size(cam_image)
 figure; imshow(cam_image);
 
 %% eliminate background
-
-
-
+% I=rgb2gray(cam_image);
+% background = imopen(I,strel('disk',45));
+% 
+% figure
+% surf(double(background(1:8:end,1:8:end))),zlim([-1 1]);
+% ax = gca;
+% ax.YDir = 'reverse';
+% 
+% figure;
+% I2 = I - background;
+% imshow(I2)
+% 
+% I3 = imadjust(I2);
+% imshow(I3);
+% 
+% bw = imbinarize(I3);
+% bw = bwareaopen(bw, 50);
+% imshow(bw)
 %% RGB
 
 rmat=cam_image(:,:,1);
@@ -108,6 +123,42 @@ title('S Plane');
 subplot(2,2,3), imshow(vi3);
 title('V Plane');
 subplot(2,2,4), imshow(HSVsum);
+title('Sum of all the planes');
+
+%% NTSC
+cam_image_ntsc=rgb2ntsc(cam_image);
+     
+     NT=cam_image_ntsc(:,:,1);
+     S=cam_image_ntsc(:,:,2);
+     C=cam_image_ntsc(:,:,3);
+     
+figure(1);
+subplot(2,2,1), imshow(NT);
+title('NT Plane');
+subplot(2,2,2), imshow(S);
+title('S Plane');
+subplot(2,2,3), imshow(C);
+title('C Plane');
+subplot(2,2,4), imshow(cam_image_ntsc);
+title('Original NTSC Image');
+
+levelh = 0.26; %higher more black
+levels = 0.1; %higher more withe
+levelv = 0.10; %higher more black, lower less black
+nti=imbinarize(NT,levelh) ;%& not(imbinarize(H,levelh2));
+si=imbinarize(S,levels);
+ci=imbinarize(C,levelv);
+NTSCsum = (nti | si | ci );
+
+figure(2);
+% Plot the data
+subplot(2,2,1), imshow(nti);
+title('N Plane');
+subplot(2,2,2), imshow(si);
+title('T Plane');
+subplot(2,2,3), imshow(ci);
+title('SC Plane');
+subplot(2,2,4), imshow(NTSCsum);
 title('Sum of all the planes');
 
 %% Complement Image and Fill in holes
