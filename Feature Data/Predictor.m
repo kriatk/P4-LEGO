dbcd2a24b213316c4a4a364b317083e4d7613095
kriatk <kristijonas.atk@gmail.com>
2018-05-11 14:08:20 +0200

    SVM classifier

--- Feature Data/Predictor.m
+++ Feature Data/Predictor.m
@@ -1,14 +1,18 @@
 clear all; close all; clc; 
-
+% This code is an example of how to predict class of an extracted blob
+%Load feature space and test image
 load Feature_space;
 img = imread('testImage.jpg');
 
+% This image filetring is bad, but can be exchanged with some Stefan code
+% or cascade classifier.
 img_gray = rgb2gray(img);
 level = graythresh(img_gray);
 img_bw = imbinarize(img_gray,'global');
 img_bw = imfill(img_bw, 'holes');
 imshow(img_bw);
 
+%Extract some info about the blobs & remove not allowable blobs
 stats = regionprops(img_bw, img_gray, 'Area', 'MajorAxisLength', 'MinorAxisLength', 'ConvexArea', 'Eccentricity', 'EquivDiameter', 'Perimeter', 'Solidity', 'MeanIntensity');
 
 minimumSize = 8000;
@@ -26,5 +30,6 @@ end
 
 blobs = struct2table(stats);
 
+% train the classifier and make predcitions 
 [trainedClassifier, validationAccuracy] = trainClassifier(feature_space) %Quadratic SVM classifier function
 yfit = trainedClassifier.predictFcn(blobs) % 1 Red; 2 Gray; 3 Car;
--- Feature Data/feature_consilidator.m
+++ Feature Data/feature_consilidator.m
@@ -1,12 +1,15 @@
 clear all; close all; clc;
 
+% These were aquired with Iterate_through_images.m code 11/05/2018
 red = importdata('Red_features.mat');
 gray = importdata('Gray_features.mat');
 car = importdata('Car_features.mat');
 
+% Conssilidate the table to aquire complete feature space
 feature_space = [red;gray;car];
-label = zeros(1, height(feature_space));
 
+% Add class labels for supervised learning
+label = zeros(1, height(feature_space));
 for i=1:height(red)+1
     label(i) = 1;
 end
@@ -16,7 +19,6 @@ end
 for i=i:(i+height(car))-1
     label(i) = 3;
 end
-
 label = label'
 feature_space.Label = label;
 
